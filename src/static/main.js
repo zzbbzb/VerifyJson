b526250b0b4550492814100c6f4b5f84cae2102a
zb <bin.zhou@k7.cn>
2019-10-18 14:56:51 +0800

    feat:增加编辑json, 保存编辑

--- src/static/main.js
+++ src/static/main.js
@@ -1,6 +1,9 @@
 var dropZone = document.getElementById('jsonContainer');
 var info = document.getElementById('info');
 
+var infoJson = undefined;
+var fileName = ".json";
+
 function handleFileDragStart(e) {
     e.stopPropagation();
     e.preventDefault();
@@ -26,6 +29,7 @@ function handleFileDragOver(e) {
 function handleFileDrop(e) { // TODO 代码优化
     e.stopPropagation();
     e.preventDefault();
+    infoJson = undefined;
 
     this.classList.remove('hovering');
 
@@ -49,6 +53,7 @@ function handleFileDrop(e) { // TODO 代码优化
     // 判断文件是json结尾
     var pattern = /.json/i;
     var file = files[0];
+    fileName = file.name;
     if (!pattern.test(file.name)) {
         info.classList.add('error');
         info.innerHTML = "需要放入一个.json结尾文件";
@@ -70,37 +75,105 @@ function handleFileDrop(e) { // TODO 代码优化
     reader.onload = function() {
         var fileText = reader.result;
         // var jsonText = JSON.stringify(fileText);
-        var infoJson = JSON.parse(fileText);
-        info.innerHTML = "成功读取文件,共" + infoJson.length + "个";
+        infoJson = JSON.parse(fileText);
+        info.innerHTML = "成功读取文件,共" + GetJsonLength() + "个";
         var str = "";
-
-        for (var i = 0; i < infoJson.length; i++) {
-            str += infoJson[i].skillId + "<br/>";
-            str += infoJson[i].skillType + "<br/>";
-            str += infoJson[i].value + "<br/>";
-            str += "=================" + "<br/>";
-        }
         // dropZone.innerHTML = str;
 
         // 进行布局
-        JsonLayout(infoJson);
+        JsonLayout();
     };
 };
 
-function JsonLayout(infoJson) {
-    var html = "";
-    for (var i = 0; i < infoJson.length; i++) {
-        html += "<div id='jsonDiv'>";
-        html += "<div id='singleContainer'>"
-            // TODO 进行布局
-        for (var key in infoJson[i]) {
-            html += "<b>" + key + ":</b>" + "  <input type='text' value='" + infoJson[i][key] + "'><br/>";
+function GetJsonLength() {
+    if (infoJson.length == undefined) {
+        var count = 0;
+        for (var key in infoJson) {
+            count++;
         }
-        html += "</div>";
-        html += "</div>";
+        return count;
+    } else {
+        return infoJson.length;
+    }
+}
+
+function GetJsonType(json) {
+    if (json.length == undefined) {
+        return 1; // 类
+    } else {
+        return 0; // 数组
+    }
+}
+
+function JsonLayout() {
+    var html = "";
+    if (typeof(infoJson) == "undefined") {
+        return;
+    }
+
+    // TODO 布局优化,适应各种json
+
+    if (GetJsonType(infoJson) == 0) {
+        for (var i = 0; i < GetJsonLength(); i++) {
+            html += "<div id='jsonDiv'>";
+            html += "<div id='jsonHead'>";
+            html += "<button class='btn' id='btn_" + i + "' onclick='PressBtn(" + i + ")'>编辑</button>";
+            html += "</div>";
+            html += "<div id='singleContainer'>";
+            // 进行布局
+            for (var key in infoJson[i]) {
+                html += "<lable for='" + key + "_" + i + "'><b>" + key + ":</b></lable>" +
+                    "  <input id='" + key + "_" + i + "' type='text' value='" + infoJson[i][key] + "' readonly='readonly' disabled='disabled'><br/>";
+            }
+            html += "</div>";
+            html += "</div>";
+        }
     }
     dropZone.innerHTML = html;
 };
+
+function PressBtn(i) {
+    var btnName = "btn_" + i;
+
+    var btn = document.getElementById(btnName);
+    if (btn.innerText == '编辑') {
+        for (var key in infoJson[i]) {
+            var keyName = key + "_" + i;
+            document.getElementById(keyName).removeAttribute('readonly');
+            document.getElementById(keyName).removeAttribute('disabled');
+        }
+        btn.innerText = '完成';
+    } else if (btn.innerText == '完成') {
+        for (var key in infoJson[i]) {
+            var keyName = key + "_" + i;
+            document.getElementById(keyName).readOnly = "readonly";
+            document.getElementById(keyName).disabled = "disabled";
+        }
+
+        for (var key in infoJson[i]) {
+            var keyName = key + "_" + i;
+            var jsonValue = document.getElementById(keyName).value;
+            infoJson[i][key] = jsonValue;
+        }
+        btn.innerText = '编辑';
+    }
+}
+
+function SaveJson() {
+    // var content = "这是直接使用HTML5进行导出的";
+    // var blob = new Blob([content], { type: "text/plain;charset=utf-8" });
+    // saveAs(blob, "file.txt");
+    // function createAndDownloadFile(fileName, content) {
+
+    // }
+    var aTag = document.createElement('a');
+    var jsonStr = JSON.stringify(infoJson);
+    var blob = new Blob([jsonStr]);
+    aTag.download = fileName;
+    aTag.href = URL.createObjectURL(blob);
+    aTag.click();
+    URL.revokeObjectURL(blob);
+}
 
 dropZone.addEventListener('dragenter', handleFileDragEnter, false);
 dropZone.addEventListener('dragleave', handleFileDragLeave, false);
